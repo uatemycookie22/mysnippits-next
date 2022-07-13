79e9daefc357673f8d230acb262bc72e748c1d5f
uatemycookie22 <pairofkings30@gmail.com>
2022-07-12 21:51:47 -0500

    innit snippit tree

---
+++ pages/manager/manager-body/snippit-tree/snippit-tree.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+
+const SnippitTree: FunctionComponent = () => {
+    return (
+        <div className="tree">
+            <p>Tree!</p>
+        </div>
+    )
+}
+
+export default SnippitTree;
