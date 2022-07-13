6dd91e6c43eb14a2e5a1c5844095c19129462d59
uatemycookie22 <pairofkings30@gmail.com>
2022-07-12 21:24:15 -0500

    implement header

--- pages/manager/manager-header/manager-header.tsx
+++ pages/manager/manager-header/manager-header.tsx
@@ -1,7 +1,11 @@
-export default function Header(){
+import {FunctionComponent} from "react";
+
+const Header: FunctionComponent = () => {
     return (
         <div className="header">
             <p>Header!</p>
         </div>
     )
 }
+
+export default Header;
