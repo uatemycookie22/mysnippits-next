3aa62894b1eea7d4cbdd35a736c16b250beb88be
uatemycookie22 <pairofkings30@gmail.com>
2022-07-12 21:46:27 -0500

    innit manager body

---
+++ pages/manager/manager-body/manager-body.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+
+const Body: FunctionComponent = () => {
+    return (
+        <div className="body">
+            <p>Body!</p>
+        </div>
+    )
+}
+
+export default Body;
