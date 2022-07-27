eb2dd4ac07075630347da2244df6e20a0c24e04e
uatemycookie22 <pairofkings30@gmail.com>
2022-07-26 22:32:31 -0500

    bind folder data to display on snippit tree

--- libs/components/snippit-tree/snippit-tree.tsx
+++ libs/components/snippit-tree/snippit-tree.tsx
@@ -1,11 +1,19 @@
 import {FunctionComponent} from "react";
-import {TreeProps} from "utils";
+import {FolderCollection, TreeProps} from "utils";
 
-export const SnippitTree: FunctionComponent<TreeProps> = () => {
+function constructFolderList<FunctionComponent>(folderCollection: FolderCollection){
+    return folderCollection.folders.map(folder => {
+        return (<li key={folder.folderName}>{folder.folderName}</li>)
+    })
+}
+
+export const SnippitTree: FunctionComponent<TreeProps> = (props) => {
+    const folderList = constructFolderList(props.folderCollection)
+
     return (
         <div className="tree">
             <ul>
-
+                {folderList}
             </ul>
         </div>
     )
--- pages/manager/manager-body/manager-body.tsx
+++ pages/manager/manager-body/manager-body.tsx
@@ -1,16 +1,19 @@
 import {FunctionComponent} from "react";
 import {SnippitTree, EmptyTree} from "components"
-import {TreeProps} from "utils";
+import {getTreePropsSample, TreeProps} from "utils";
 
 const getTreeOrEmpty: FunctionComponent<TreeProps> = (props) => {
-    return false ? (<SnippitTree {...props} ></SnippitTree>) : (<EmptyTree></EmptyTree>)
+    return props.folderCollection.folders.length ?
+        (<SnippitTree {...props} ></SnippitTree>) : (<EmptyTree></EmptyTree>)
 }
 
 export default function Body<FunctionComponent>() {
+    const treeOrEmpty = getTreeOrEmpty(getTreePropsSample())
+
     return (
         <div className="body">
             <p>Body!!!</p>
-            {getTreeOrEmpty({})}
+            {treeOrEmpty}
         </div>
     )
 }
