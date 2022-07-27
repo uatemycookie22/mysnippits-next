0f201898a4d24a3865ca69fc7b8ce1941182b34b
uatemycookie22 <pairofkings30@gmail.com>
2022-07-26 22:31:25 -0500

    initialize interfaces for snippit tree

--- libs/utils/snippit-tree/snippit-tree.utils.ts
+++ libs/utils/snippit-tree/snippit-tree.utils.ts
@@ -1,3 +1,44 @@
-export interface TreeProps {
+interface FileName {
+    rawName: string,
+    name: string,
+    extension: string,
+}
 
+interface Snippit {
+    fileNameMetadata: FileName
+}
+
+interface SnippitCollection {
+    snippits: Snippit[]
+}
+
+interface Folder {
+    folderName: string
+    snippitCollection: SnippitCollection
+}
+
+export interface FolderCollection {
+    folders: Folder[]
+}
+
+export interface TreeProps {
+    folderCollection: FolderCollection
+}
+
+export function getTreePropsSample(): TreeProps {
+    const snippitsSample: SnippitCollection = {
+        snippits: []
+    }
+
+    const foldersSample: FolderCollection = {
+        folders: [
+            {folderName: 'My folder 1!', snippitCollection: snippitsSample},
+            {folderName: 'My folder 2!', snippitCollection: snippitsSample},
+            {folderName: 'My folder 3!', snippitCollection: snippitsSample},
+        ],
+    }
+
+    return {
+        folderCollection: foldersSample
+    }
 }
